d4bb6cdca0e7438f17b33c078c75cf4c9636cd95
iamsaadatkhan <50053668+iamsaadatkhan@users.noreply.github.com>
2020-11-06 22:44:20 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// centered h2 gradeient
-var originalBG = $(".centered h2").css("background");
+// circle cursor
+jQuery(document).ready(function () {
+  var mouseX = 0,
+    mouseY = 0;
+  var xp = 0,
+    yp = 0;
 
-$('.centered h2')
-  .mousemove(function(e) {
-
-    x = e.pageX - this.offsetLeft;
-    y = e.pageY - this.offsetTop;
-    xy = x+y ;
-    bgWebKit = "linear-gradient(" + xy + "deg, #4ac1ff, #795bb0)";
-
-    $(this).css({
-      'background': bgWebKit
-    })
-  }).mouseleave(function() {
-    $(this).css({
-      background: originalBG
-    });
+  $(document).mousemove(function (e) {
+    mouseX = e.pageX - 30;
+    mouseY = e.pageY - 30;
   });
+
+  setInterval(function () {
+    xp += (mouseX - xp) / 6;
+    yp += (mouseY - yp) / 6;
+    $("#circle").css({ left: xp + "px", top: yp + "px" });
+  }, 20);
+});
+
 
   // center-right
 
@@ -43,22 +43,3 @@ $('.centered h2')
 
 
 // Move the cursor
-jQuery(document).ready(function() {
-
-    var mouseX = 0, mouseY = 0;
-    var xp = 0, yp = 0;
-     
-    $(document).mousemove(function(e){
-      mouseX = e.pageX - 30;
-      mouseY = e.pageY - 30; 
-    });
-      
-    setInterval(function(){
-      xp += ((mouseX - xp)/6);
-      yp += ((mouseY - yp)/6);
-      $("#circle").css({left: xp +'px', top: yp +'px'});
-    }, 20);
-  
-  });
-
- 
